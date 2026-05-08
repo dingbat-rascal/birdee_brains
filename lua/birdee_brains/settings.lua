457bd65cbb00749bd09434483c234549506a36c0
dingbat-rascal <dingbat.rascal@gmail.com>
2026-05-08 07:40:00 -0700

    refactor: extract settings to separate module

---
+++ lua/birdee_brains/settings.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+M.DEFAULTS = {
+    -- You can also point both bird_dir to the same ex (english, and english) to get a typing trainer.
+    bird_a         = "spanish",
+    bird_b         = "english",
+    course_number  = 1,
+
+    game_mode      = "multiple_choice",
+    -- options "speedrun" or multiple_choice
+    reveal_correct = true,
+    reinforce      = true, -- reinforce things you get wrong
+
+    input_keymap   = "german-qwertz",
+
+    -- empty "" for english or view available with
+    -- :echo globpath(&rtp, "keymap/*.vim")
+    -- stored default list in available
+    -- or you can use custom ones from ./nvim/keymap/example.vim dir
+    -- you dont need full path just example.vim
+}
+
+return M
